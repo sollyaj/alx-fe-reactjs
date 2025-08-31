3097fdb98c00a59af9edf0d41c7632cb2a6f384f
sollyaj <soajohworode@gmail.com>
2025-08-31 21:31:21 +0100

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,9 +7,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
       <div>
+        <WelcomeMessage /> 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       
     </>
   )
 }
