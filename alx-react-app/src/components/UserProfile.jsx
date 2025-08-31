2d399148b91c02a30344365b2896d62a7e3935b4
Solomon <soajohworode@gmail.com>
2025-08-31 23:41:29 +0100

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile(props) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
-    </div>
+       <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+     </div>
   );
 }
 
