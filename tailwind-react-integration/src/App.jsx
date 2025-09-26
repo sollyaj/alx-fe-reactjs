18acd4319a105d7fb1d69c138c89fde84123d0eb
sollyaj <soajohworode@gmail.com>
2025-09-27 00:23:24 +0100

    feat: add responsive user profile card with adaptive typography, spacing, and image sizing

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+
+  return (
+    <>
+    <UserProfile/>
+    </>
+  )
+}
+
+export default App
