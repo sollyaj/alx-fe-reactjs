2832ddce1245e28bc9c5f5525d56c3ed098f2752
sollyaj <soajohworode@gmail.com>
2025-10-12 23:53:44 +0100

    Integrated React Router with BrowserRouter, Routes, and Route for PostsComponent

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // or 'react-query'
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
@@ -17,9 +18,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 function App() {
   return (
-    <div className="App">
-      <PostsComponent />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostsComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
