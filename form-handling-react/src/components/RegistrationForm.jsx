7f88510aeb13c9f70039b243277ba433ea2853b2
sollyaj <soajohworode@gmail.com>
2025-10-12 23:06:46 +0100

    update

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -48,7 +48,7 @@ const RegistrationForm = () => {
           type="text"
           name="username"
           placeholder="Username"
-          value={formData.username}
+          value={username}
           onChange={handleChange}
           className="w-full p-2 border rounded"
         />
@@ -56,7 +56,7 @@ const RegistrationForm = () => {
           type="email"
           name="email"
           placeholder="Email"
-          value={formData.email}
+          value={email}
           onChange={handleChange}
           className="w-full p-2 border rounded"
         />
@@ -64,7 +64,7 @@ const RegistrationForm = () => {
           type="password"
           name="password"
           placeholder="Password"
-          value={formData.password}
+          value={password}
           onChange={handleChange}
           className="w-full p-2 border rounded"
         />
